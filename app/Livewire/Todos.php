ac18c29d16d4743b98f1f386d64d70b03146190a
Frank M'PO <tcheytiba1999@gmail.com>
2024-03-02 18:36:03 +0100

    Lifecycle hooks: mount and updated | updates$Property functions

--- app/Livewire/Todos.php
+++ app/Livewire/Todos.php
@@ -6,11 +6,8 @@ use Livewire\Component;
 
 class Todos extends Component
 {
-    public $todo = 'eat';
-    public $todos = [
-        'Trash',
-        'Dishes',
-    ];
+    public $todo = '';
+    public $todos = [];
 
     public function add()
     {
@@ -18,6 +15,25 @@ class Todos extends Component
         //$this->todo = '';
         $this->reset('todo');
     }
+
+    public function mount()
+    {
+        $this->todos = ['Trash','Dishes',];
+        $this->todo = 'Type...';
+    }
+
+    //this is called at every update of a wire-model value
+    //can be more easy for only $todo with updatedTodo
+    #public function updated($property, $value)
+
+    public function updatedTodo($value)
+    {
+        #$this->$property = strtoupper($value);
+        $this->todo = strtoupper($value);
+
+        //this is usable for validating like $this->validate()
+    }
+
     public function render()
     {
         return view('livewire.todos');
