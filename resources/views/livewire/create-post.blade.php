9f4555dd865d3745de35dccaa32aca2a007be93f
Frank M'PO <tcheytiba1999@gmail.com>
2024-03-02 21:32:53 +0100

    Alpine: submit the form with alpine, call wire property without request to server

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,5 +1,6 @@
 <div>
     <form wire:submit="save">
+
         <div class="form-group">
             <label for="title">Title</label>
             <input type="text" wire:model="title" class="form-control">
@@ -10,6 +11,12 @@
         <div class="form-group mt-3">
             <label for="content">Content</label>
             <textarea name="" id="" cols="30" rows="10" wire:model="content" class="form-control"></textarea> 
+            <small>
+                Characters: <span x-text="$wire.content.length"></span>
+            </small> <br>
+            <small>
+                Words: <span x-text="$wire.content.split(' ').length - 1"></span>
+            </small> <br>
             @error('content')
                 <span class="text-danger text-sm font-italic">{{ $message }}</span>
             @enderror
@@ -18,4 +25,6 @@
             <button type="submit" class="btn btn-primary">Save</button>
         </div>
     </form>
+    <br>
+    <button type="button" class="btn btn-secondary" x-on:click="$wire.save()">Submit</button>
 </div>
